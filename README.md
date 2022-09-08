7b4b400597882f1dc1b295a970f31bc9f2a40456
Aliyu Usman <usman.m1604342@st.futminna.edu.ng>
2022-09-08 12:08:18 +0100

    useRouter querying

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Play with Next.js
+
+> Play with Next.Js as thought by Scott Moss @Frontend Masters
--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React from "react";
 
-function id() {
-  return (
-    <div>[id]</div>
-  )
+function Page() {
+  const router = useRouter();
+
+  const { id } = router.query;
+  return <div>Page { id }</div>;
 }
 
-export default id
+export default Page;
